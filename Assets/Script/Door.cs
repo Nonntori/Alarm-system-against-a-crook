e65bf443dead9b96d772a72ada80740a31a8a3da
Ilya <kamad0tanjiro@outlook.com>
2022-06-22 11:32:35 +0300

    Changing the name of the script

--- Assets/Script/Door.cs
+++ Assets/Script/Door.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class MotionDetector: MonoBehaviour
+public class Door: MonoBehaviour
 {
     [SerializeField] UnityEvent _movementEnter;
     [SerializeField] UnityEvent _movementLeave;
