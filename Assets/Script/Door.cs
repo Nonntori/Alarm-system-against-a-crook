4f6ed4c89ee59af9b74ff5ff27cdf5d462dddfa4
Ilya <kamad0tanjiro@outlook.com>
2022-06-23 13:38:33 +0300

    Update Door.cs

--- Assets/Script/Door.cs
+++ Assets/Script/Door.cs
@@ -6,14 +6,18 @@ public class Door: MonoBehaviour
     [SerializeField] UnityEvent _movementEnter;
     [SerializeField] UnityEvent _movementLeave;
 
+    public static bool IsMovement;
+
     private void OnTriggerEnter(Collider other)
     {
+        IsMovement = true;
         _movementEnter?.Invoke();
     }
 
     private void OnTriggerExit(Collider other)
     {
-        _movementLeave?.Invoke();
+        IsMovement = false;
+        _movementLeave?.Invoke(); 
     }
 }
 
